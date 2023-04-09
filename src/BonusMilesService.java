9dfc0b2bcbb1a1f5bfaf38a273d9da68836731b2
Nikita Kruglov <bybalesh234@gmail.com>
2023-04-09 15:36:07 +0600

    Second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-public int calculate (int price) {
-    int flight = 20;
-    int calculate = (price / flight);
-    return calculate;
-}
+    public int calculate(int price) {
+        int flight = 20;
+        int calculate = (price / flight);
+        return calculate;
+    }
 }
